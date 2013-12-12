4187cce8e5d4f83ca5c41bffc49465c2ab78b37c
simplay <michael.single@students.unibe.ch>
2013-12-12 14:53:19 +0100

    Prototyped alignment
    + implemented prototyp featureshift to zero
    + defined header of method rescale ears to one.

--- src/assignment7/PostprocessAlignment.java
+++ src/assignment7/PostprocessAlignment.java
@@ -1,11 +1,42 @@
 package assignment7;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.vecmath.Point3f;
+import javax.vecmath.Vector3f;
+
+import meshes.Vertex;
+import meshes.WireframeMesh;
+
 public class PostprocessAlignment {
-	public PostprocessAlignment(){
+	private WireframeMesh wireframeMesh;
+	private List<Vertex> features;
+
+	public PostprocessAlignment(WireframeMesh mesh, List<Vertex> featurelist){
+		this.wireframeMesh = mesh;
+		this.features = featurelist;
+		shiftFeaturesToAvgZero();
+		rescaleEarDistanceToOne();
 		
 	}
 	
-	private performShift(){
+	private void shiftFeaturesToAvgZero(){
+		float averageShiftAmount = 0.0f;
+		for(Vertex feature : features){
+			Point3f p = feature.getPos();
+			float delta = (float) (Math.sqrt(p.x*p.x* + p.y*p.y* + p.z*p.z));;
+			averageShiftAmount += delta;
+		}
+		
+		Vector3f posShow = new Vector3f(averageShiftAmount,averageShiftAmount,averageShiftAmount);
+		ArrayList<Point3f> positions = wireframeMesh.vertices;
+		for(Point3f position : positions){
+			position.sub(posShow);
+		}
+	}
+	
+	private void rescaleEarDistanceToOne(){
 		
 	}
 }
