b30196040b14c72d80c459f3b9857551824b3877
simplay <michael.single@students.unibe.ch>
2013-12-12 17:15:39 +0100

    Fixed Constructor for PostprocessAlignment
    + Loading all meshes and features correctly.
    + using first mesh and featuresList as reference entities.
    + validity check for PostprocessAlignment which throws an exceptions
    iff conditions do not hold true.

---
+++ src/assignment7/AlignmentProcessor.java
@@ -0,0 +1,13 @@
+package assignment7;
+
+public class AlignmentProcessor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/assignment7/PostprocessAlignment.java
+++ src/assignment7/PostprocessAlignment.java
@@ -10,29 +10,57 @@ import meshes.Vertex;
 import meshes.WireframeMesh;
 
 public class PostprocessAlignment {
-	private WireframeMesh wireframeMesh;
-	private List<Vertex> features;
-
-	public PostprocessAlignment(WireframeMesh mesh, List<Vertex> featurelist){
-		this.wireframeMesh = mesh;
-		this.features = featurelist;
-		shiftFeaturesToAvgZero();
-		rescaleEarDistanceToOne();
-		
-	}
+	private WireframeMesh baseMesh;
+	private List<WireframeMesh> meshList;
+	private List<Vertex> baseFeatures;
+	private List<List<Vertex>> featuresList;
 	
-	private void shiftFeaturesToAvgZero(){
-		float averageShiftAmount = 0.0f;
-		for(Vertex feature : features){
-			Point3f p = feature.getPos();
-			float delta = (float) (Math.sqrt(p.x*p.x* + p.y*p.y* + p.z*p.z));;
-			averageShiftAmount += delta;
+	/**
+	 * Performs allignment
+	 * Assumption: first element of meshList and featuresList is the reference mesh for the whole allignment process.
+	 * @param mesheList
+	 * @param featuresList
+	 * @throws Exception throws an exception if there are less than two meshes,featuresList or they have not same amount of elements contained
+	 */
+	public PostprocessAlignment(List<WireframeMesh> meshList, List<List<Vertex>> featuresList) throws Exception{
+		this.baseMesh = meshList.get(0);
+		this.meshList = meshList;
+		this.baseFeatures = featuresList.get(0);
+		this.featuresList = featuresList;
+		
+		if(!validInput()){
+			throw new Exception("incorrect input");
 		}
 		
-		Vector3f posShow = new Vector3f(averageShiftAmount,averageShiftAmount,averageShiftAmount);
-		ArrayList<Point3f> positions = wireframeMesh.vertices;
-		for(Point3f position : positions){
-			position.sub(posShow);
+		shiftFeaturesToAvgZero();
+		rescaleEarDistanceToOne();		
+	}
+	
+	private boolean validInput(){
+		boolean sameCount = meshList.size() == featuresList.size();
+		boolean atLeastTwo = meshList.size() > 1 && featuresList.size() > 1;
+		return sameCount&&atLeastTwo;
+	}
+	
+	/**
+	 * center each mesh to avg position zero
+	 */
+	private void shiftFeaturesToAvgZero(){
+		int counter = 0;
+		for(List<Vertex> features : featuresList){
+			float averageShiftAmount = 0.0f;
+			for(Vertex feature : features){
+				Point3f p = feature.getPos();
+				float delta = (float) (Math.sqrt(p.x*p.x* + p.y*p.y* + p.z*p.z));;
+				averageShiftAmount += delta;
+			}
+			averageShiftAmount /= features.size();
+			Vector3f posShow = new Vector3f(averageShiftAmount, averageShiftAmount, averageShiftAmount);
+			ArrayList<Point3f> positions = meshList.get(counter).vertices;
+			for(Point3f position : positions){
+				position.sub(posShow);
+			}
+			counter++;
 		}
 	}
 	
